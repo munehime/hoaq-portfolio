2171ffcdb954a3938eb6a59f32ad3c733db7c8b3
Mune <mune@mune.moe>
2025-01-08 10:34:44 +0700

    chore: update packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
     extensions: [".svelte"],
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            runtime: "nodejs20.x",
+        }),
     },
     preprocess: vitePreprocess(),
 };
